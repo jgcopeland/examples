65a927fae0729b03602fd8fc6ed8ef3e47668aef
jgcopeland <150083388+jgcopeland@users.noreply.github.com>
2023-11-30 14:36:59 +0000

    Added main() section to show function of setResetBit.h

--- Bitwise Examples/binExamples.cpp
+++ Bitwise Examples/binExamples.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "countBinOnes.h"
+#include "setResetBit.h"
 using namespace std;
 
 //File used as main.cpp to call all binary example functions
@@ -7,6 +8,9 @@ using namespace std;
 //How to do correctly? use header file? use extern somehow?
 
 int main() {
+
+// countBinOnes.h
+//------------------------------------------------------------------------------------
     uint8_t num8 = 0x56; //Is hex the standard format to use for embedded applications? Or are there cases where you use bin/oct?
     cout << int(num8) << " has " << countBinOnes(num8) << " binary ones.\n";
     num8 = 0x1;
@@ -25,7 +29,17 @@ int main() {
     num16 = 12359;
     cout << int(num16) << " has " << countBinOnes(num16) << " binary ones.\n";
     num16 = 984;
-    cout << int(num16) << " has " << countBinOnes(num16) << " binary ones.\n";
+    cout << int(num16) << " has " << countBinOnes(num16) << " binary ones.\n\n";
+
+// setResetBit.h
+//------------------------------------------------------------------------------------
+    num8 = 0x56;
+    int bit = 0;
+    cout << int(num8) << " had bit " << bit << " set. It is now " << setBit(num8, bit) << ".\n";
+
+    num8 = 0x57;
+    bit = 0;
+    cout << int(num8) << " had bit " << bit << " reset. It is now " << resetBit(num8, bit) << ".\n";
 
     return 0;
 }
