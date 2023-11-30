bdd91a5f5e10131e97dd8cfba85d9462bea27daf
jgcopeland <150083388+jgcopeland@users.noreply.github.com>
2023-11-30 14:15:06 +0000

    Removed unused include for countBinOnes, and set up setResetBit structure.

---
+++ Bitwise Examples/setResetBit.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//sets the bit (=1) at the provided position (0-7) in the provided 8 bit integer
+int setBit(uint8_t num, int bitPos) {
+    return 0;
+}
+
+//resets the bit (=0) at the provided position (0-7) in the provided 8 bit integer
+int resetBit(uint8_t num, int bitPos) {
+    return 0;
+}
