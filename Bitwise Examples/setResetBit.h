a25f31a410311b17a07d3d3db4338920827e0af3
jgcopeland <150083388+jgcopeland@users.noreply.github.com>
2023-11-30 14:20:36 +0000

    First implementation of set/reset functions.

--- Bitwise Examples/setResetBit.h
+++ Bitwise Examples/setResetBit.h
@@ -3,10 +3,12 @@ using namespace std;
 
 //sets the bit (=1) at the provided position (0-7) in the provided 8 bit integer
 int setBit(uint8_t num, int bitPos) {
-    return 0;
+    num |= 1<<bitPos;
+    return num;
 }
 
 //resets the bit (=0) at the provided position (0-7) in the provided 8 bit integer
 int resetBit(uint8_t num, int bitPos) {
-    return 0;
+    num &= 0<<bitPos;
+    return num;
 }
